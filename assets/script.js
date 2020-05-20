841f6a15cd3941f395f8223f0b261370469a9e93
Kimberly Wilks <wilks.kimberly@gmail.com>
2020-05-20 00:23:07 -0500

    added time conditions

--- assets/script.js
+++ assets/script.js
@@ -2,15 +2,15 @@
 // create dynamic styling by changing class for past, future, and present times
 // on click of a save button take the input from the user and save it to local storage
 // include prevent default to ensure that input is not erased
-
+var textBlock = document.getElementsByClassName("hour");
+var labeling = document.getElementsByTagName("label");
 var now = moment().format("dddd, MMM Do, YYYY");
 $("#currentDay").text(now);
 loadInfo();
-var saved = [];
-var times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+nowOrLater();
 $("button").on("click", function (event) {
   var info = JSON.parse(localStorage.getItem("todoItems")) || [];
-  info[this.value] = document.getElementsByClassName("hour")[this.value].value;
+  info[this.value] = textBlock[this.value].value;
   //   var data = document.getElementsByClassName("hour")[i].value;
   //   console.log(data);
   localStorage.setItem("todoItems", JSON.stringify(info));
@@ -18,9 +18,27 @@ $("button").on("click", function (event) {
 });
 
 function loadInfo() {
-  saved = JSON.parse(localStorage.getItem("todoItems"));
+  var saved = JSON.parse(localStorage.getItem("todoItems"));
+  var times = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
 
   for (var i = 0; i < saved.length; i++) {
     document.getElementById(times[i]).value = saved[i];
   }
 }
+function nowOrLater() {
+  for (var i = 0; i < labeling.length; i++) {
+    //if this hour < current hour set attribute class to past
+    if (labeling[i].htmlFor < moment().format("H")) {
+      $(textBlock[i]).addClass("past");
+    }
+    //if current hour =  hour set attribute class to present
+    if (labeling[i].htmlFor === moment().format("H")) {
+      $(textBlock[i]).addClass("present");
+    }
+    //else  set attribute class to future
+    else {
+      $(textBlock[i]).addClass("future");
+    }
+  }
+}
+console.log(textBlock[0]);
